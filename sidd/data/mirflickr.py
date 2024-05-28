509d1cf14144c191c39286b0b4f2a0a35d73cada
Oleh Astappiev <astappiev@l3s.de>
2024-05-28 18:34:10 +0200

    fix: mirflickr labels

--- sidd/data/mirflickr.py
+++ sidd/data/mirflickr.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 from glob import glob
 from sidd.utils.common import get_dataset
+from sidd.utils.plot_dataset import plot_label, plot_sample
 from sidd.data import AbsDataset
 import numpy as np
 import os.path
 
-from sidd.utils.plot_dataset import plot_label, plot_sample
 
 DEFAULT_BATCH_SIZE = 32
 DEFAULT_IMAGE_SIZE = (500, 500)
@@ -53,9 +53,6 @@ class MFND(AbsDataset):
     def get_classes(self):
         raise NotImplementedError()
 
-    def get_combined(self):
-        return super().get_train()
-
     def _load_dataset(self, image_size, batch_size, map_fn):
         def load(path):
             image_raw = tf.io.read_file(path)
@@ -86,8 +83,9 @@ class MFND(AbsDataset):
         test_ds = ds.skip(ds_size * 0.6).skip(ds_size * 0.2)
         return train_ds, val_ds, test_ds
 
+
 if __name__ == "__main__":
     ds = MFND().get_combined()
 
-    #plot_sample(ds)
+    plot_sample(ds)
     plot_label(ds, 4430)
